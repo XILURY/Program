41d5c5991e7ab80648ac80a5f40d6b48178dc39e
XILURY <ysdxzxl@email.com>
2020-03-20 21:26:06 +0800

    斐波那契数列(兔子繁殖问题)+大数值的使用

--- src/RabitNumber/RabitNumber.java
+++ src/RabitNumber/RabitNumber.java
@@ -2,10 +2,13 @@
 程序分析： 兔子的规律为数列1,1,2,3,5,8,13,21....
 *
 善于看到本质内容 题目本身只是个斐波那契数列的运算。后一个数等于前两个数之和 按照此思路即可求解
-*
+注意：int的最大范围是2^31-1=2147483647,当过大时 应该使用大数值。大数值计算时速度比较慢 n=50时就要等好久
+***程序调试 成功通过
 * */
 package RabitNumber;
+import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class RabitNumber {
     public static void main(String[] arg){
@@ -14,12 +17,12 @@ public class RabitNumber {
         int n = in.nextInt();
         System.out.println("第"+n+"个兔子的数量是"+numberCount(n));
     }
-     private static int numberCount(int n){
+     private static BigInteger numberCount(int n){ //n较大时 返回值会比较大 采用大数值
         if (n<3){
-            return 1;
+            return BigInteger.valueOf(1);
         }
         else{
-            return numberCount(n-1)+numberCount(n-2);
+            return numberCount(n-1).add(numberCount(n-2));
         }
     }
 }
