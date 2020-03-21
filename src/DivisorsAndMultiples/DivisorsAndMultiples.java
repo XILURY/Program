9f83533f48118293b931e6333a20becdd10b9195
XILURY <ysdxzxl@163.com>
2020-03-21 18:15:26 +0800

    最小公倍数和最大公因数

--- src/DivisorsAndMultiples/DivisorsAndMultiples.java
+++ src/DivisorsAndMultiples/DivisorsAndMultiples.java
@@ -1,6 +1,7 @@
 /*输入两个正整数m和n，求其最大公约数和最小公倍数。
 程序分析：最大公因数可以找都被两数整除的最大的那个数。最小公倍数为两数相乘除以最大公因数
 *程序调试通过
+***参考答案提出了一种辗除法求最大公因数 不是很好理解
 * */
 package DivisorsAndMultiples;
 public class DivisorsAndMultiples {
@@ -25,36 +26,36 @@ public class DivisorsAndMultiples {
     }
 }
 //参考答案
-public class DivisorsAndMultiples{
-    public static void main(String[] args){
-        int m,n;
-        try{
-            m = Integer.parseInt(args[0]);
-            n = Integer.parseInt(args[1]);
-        }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("输入有误");
-            return;
-        }
-        max_min(m,n);
-    }
-    //求最大公约数和最小公倍数
-    private static void max_min(int m, int n){
-        int temp = 1;
-        int yshu = 1;
-        int bshu = m*n;
-        if(n<m){
-            temp = n;
-            n = m;
-            m = temp;
-        }
-        while(m!=0){
-            temp = n%m;
-            n = m;
-            m = temp;
-        }
-        yshu = n;
-        bshu /= n;
-        System.out.println(m+"和"+n+"的最大公约数为"+yshu);
-        System.out.println(m+"和"+n+"的最小公倍数为"+bshu);
-    }
-}
+//public class DivisorsAndMultiples{
+//    public static void main(String[] args){
+//        int m,n;
+//        try{
+//            m = Integer.parseInt(args[0]);
+//            n = Integer.parseInt(args[1]);
+//        }catch(ArrayIndexOutOfBoundsException e){
+//            System.out.println("输入有误");
+//            return;
+//        }
+//        max_min(m,n);
+//    }
+//    //求最大公约数和最小公倍数
+//    private static void max_min(int m, int n){
+//        int temp = 1;
+//        int yshu = 1;
+//        int bshu = m*n;
+//        if(n<m){
+//            temp = n;
+//            n = m;
+//            m = temp;
+//        }
+//        while(m!=0){   //求取最大公因数的方法思路和我不一致，其采取“辗除法”不是很好理解
+//            temp = n%m;
+//            n = m;
+//            m = temp;
+//        }
+//        yshu = n;
+//        bshu /= n;
+//        System.out.println(m+"和"+n+"的最大公约数为"+yshu);
+//        System.out.println(m+"和"+n+"的最小公倍数为"+bshu);
+//    }
+//}
