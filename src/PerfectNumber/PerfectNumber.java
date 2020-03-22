be2cd7252250b0bc57f1c5a1150da3df8bf59f3f
XILURY <ysdxzxl@163.com>
2020-03-22 17:05:32 +0800

    找完数 该数等于其因数之和

---
+++ src/PerfectNumber/PerfectNumber.java
@@ -0,0 +1,23 @@
+/*一个数如果恰好等于它的因子之和，这个数就称为"完数"。例如6=1＋2＋3.编程找出1000以内的所有完数。
+* 题目比较简单
+*/
+package PerfectNumber;
+public class PerfectNumber {
+    public static void main(String[] arg){
+        int m = 1000000000;
+        for(int i=1;i<m;i++){
+            if(i == Count(i))
+                System.out.println(i);
+        }
+    }
+    //计算某数的因数和(不含本身)
+    static int Count(int i){
+        int s = 0;
+        for(int j=1;j<i;j++){
+            if(i%j==0) {
+                s = s + j;
+            }
+        }
+        return s;
+    }
+}
