8608be10a51d6e5ace9a17de34606db33fc0a9f8
XILURY <ysdxzxl@163.com>
2020-03-26 16:08:17 +0800

    *选择排序 注意数组复制只能用copyOf和键盘输入数组的方法

---
+++ src/exchange/Exchange.java
@@ -0,0 +1,56 @@
+/*选择排序输入数组，最大的与第一个元素交换，最小的与最后一个元素交换，输出数组。
+ * 用数组复制的方法 先扩大数组 再裁掉 我觉得相对简单
+ * 两个错误让我花了很多时间 一个是hasNextInt，没有int 一直循环执行 另一个是数组的最后一个元素是n-1而不是n
+ */
+package exchange;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exchange {
+    public static void main(String[] arg){
+        System.out.print("请输入一组数：");
+        Scanner scan = new Scanner(System.in).useDelimiter("\\s");
+        int[] a = new int[50];
+        int m = 0;
+        while(scan.hasNextInt()){
+            a[m++] = scan.nextInt();
+        }
+        scan.close();
+        int[] b = new int[m];
+        for(int i=0;i<m;i++)
+            b[i] = a[i];
+        ///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!玄学了 不知道为什么错了 运行不了 明明一样（可算找到了 微笑脸）
+//        System.out.print("请输入一组数字：");
+//        Scanner in= new Scanner(System.in).useDelimiter("\\s");
+//        int[] a = new int[50];
+//        int m = 0;
+//        while(in.hasNext()){     ////！！错误 应该是hasNextInt少了几个单词 程序一直疯狂执行
+//            a[m++] = in.nextInt();
+//        }
+//        in.close();
+//        int[] na = new int[m];
+//        for(int i=0;i<m;i++)
+//            na[i] = a[i];
+// /////////////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111
+        // 测试 移动5位！
+        int n = 5;
+        System.out.println("原始数组为："+Arrays.toString(b));
+        exchange(b,n);
+    }
+    static void exchange(int[] a,int n){
+        int len = a.length+n;
+        int[] xa = Arrays.copyOf(a,len);
+        //System.out.println(Arrays.toString(xa));
+        for(int i=0;i<a.length;i++){
+                xa[i+n] = a[i];
+        }
+        //System.out.println(Arrays.toString(xa));
+        for(int i =0;i<n;i++){
+            xa[i] = xa[len-n+i];    //!!注意 数组的最后一位是n-1 直接n会报溢出错误！！
+        }
+        //System.out.println(Arrays.toString(xa));
+        int[] nxa = Arrays.copyOf(xa,len-n);
+        System.out.println("移动后的数组为："+Arrays.toString(nxa));
+    }
+
+}
