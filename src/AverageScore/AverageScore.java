689b2e703c1aea1abfe86e7376018b74e7eb3a75
XILURY <ysdxzxl@163.com>
2020-03-27 21:25:03 +0800

    创建一个方法计算各科平均分

--- src/AverageScore/AverageScore.java
+++ src/AverageScore/AverageScore.java
@@ -1,5 +1,5 @@
-/*03.27 通过类的方法 实现学生总分的计算 熟悉类的和对象的使用
-*
+/*version1 通过类的方法 实现学生总分的计算 熟悉类的和对象的使用
+*version2 创建一个方法计算各科平均分
 * */
 package  AverageScore;
 import java.util.Arrays;
@@ -12,19 +12,35 @@ public class AverageScore {
         ss[2] = new Student("LL", 3, 86, 75 , 80);
         ss[3] = new Student("HH", 4, 88, 82 , 88);
         ss[4] = new Student("JJ", 5, 91, 82 , 85);
+        Double[] gra1 = new Double[5];
+        Double[] gra2 = new Double[5];
+        Double[] gra3 = new Double[5];
         Double[] total = new Double[5];
         for(int i=0;i<5;i++){
             System.out.println(ss[i].getStudentID()+"号学生"+ss[i].getName()+"总分为："+ss[i].total());
+            // 依次将学科1，学科2，学科3和总分放入数组中 便于后续操作
+            gra1[i] = ss[i].getGrade1();
+            gra2[i] = ss[i].getGrade2();
+            gra3[i] = ss[i].getGrade3();
             total[i] = ss[i].total();
         }
-        System.out.println(Arrays.toString(total));
-//        for(Student s:ss){
-//            System.out.println(s.getStudentID()+"号学生"+s.getName()+"总分为："+s.total());
-//            //System.out.println(s.getGrade1());
-//        }
-    }
-}
+        System.out.println("科目1平均分为："+average(gra1));
+        System.out.println("科目2平均分为："+average(gra2));
+        System.out.println("科目3平均分为："+average(gra3));
 
+    }
+    // 计算每科平均分的方法
+    static double average(Double[] gra){
+        double sum = 0;
+        double avar = 0;
+        for(int i=0;i<gra.length;i++){
+            sum = sum + gra[i];
+        }
+        avar = sum/gra.length;
+        return avar;
+        }
+}
+// 创建学生类
 class Student{
     private String name;
     private long studentID;
